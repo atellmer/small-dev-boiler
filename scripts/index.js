5b71fd89f5f8a69dd3f9edcb255f3cd7dc7ec50d
atellmer <atellmer@gmail.com>
2017-02-18 12:47:25 +0500

    update boiler

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
+import '../index.html';
 import Human from './test';
 
 
-const human = new Human('Alex', 27);
+const human = new Human('Alex', 28);
 console.log('Hello: ', human.name);
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
 'use strict';
 
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-	resolve: {
-		moduleDirectories: ['node_modules'],
-		extensions: ['', '.js']
-	},
-	entry: path.resolve(__dirname, 'scripts/index.js'),
-	output: {
-		path: './dist/',
-		filename: 'app.bundle.js',
-	},
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			loaders: ['babel'],
-		}]
-	},
-	plugins: [
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.NoErrorsPlugin(),
-	],
-	watchOptions: {
-		aggregateTimeout: 100
-	}
+const webpackConfig = {
+  devtool: 'eval',
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.html']
+  },
+  entry: path.resolve(__dirname, 'scripts/index.js'),
+  output: {
+    path: '/dist/',
+    filename: 'app.bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: ['babel-loader'] },
+      { test: /\.html$/, use: ['raw-loader'] }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ],
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  stats: {
+    assets: true,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: true,
+    chunks: true,
+    chunkModules: false
+  }
 }
+
+module.exports = webpackConfig;
